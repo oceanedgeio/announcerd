f988b6d7c27269b39dd4b33bbf792345372bffed
egee-irl <brian@egee.io>
2019-06-08 21:49:40 -0700

    pubsub

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -1,22 +1,31 @@
-import moment = require("moment");
 import { Client} from "discord.js";
+import moment from "moment";
 import { GetChannel, GetGuild, SendChannelMessage } from "./helpers";
+
+import Redis from "ioredis";
+
 
 export default async function Connect() {
   const client = new Client();
+
   await client.login(process.env.TOKEN);
   console.log(`Logged in at: ${moment().format("MMMM Do YYYY, h:mm:ss a")}`);
   Listen(client);
 }
 
 async function Listen(client: Client) {
-  const guild = await GetGuild(client, process.env.SERVER_ID!);
-  client.on("guildMemberAdd", async (member) => {
-    const general = await GetChannel(guild, process.env.GENERAL_CHANNEL!);
-    SendChannelMessage(general, `**${member.displayName}** has joined the server!`);
+  // const guild = await GetGuild(client, process.env.SERVER_ID!);
+  const redis = new Redis();
+  const pub = new Redis();
+
+  redis.subscribe("minecraft", (err, count) => {
+    // Now we are subscribed to both the 'news' and 'music' channels.
+    // `count` represents the number of channels we are currently subscribed to.
   });
-  client.on("guildMemberRemove", async (member) => {
-    const debug = await GetChannel(guild, process.env.DEBUG_CHANNEL!);
-    SendChannelMessage(debug, `**${member.displayName}** has left the server`);
+
+  redis.on("message", (channel, message) => {
+    // Receive message Hello world! from channel news
+    // Receive message Hello again! from channel music
+    console.log("Receive message %s from channel %s", message, channel);
   });
 }
