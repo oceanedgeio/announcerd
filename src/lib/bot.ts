b823a27a90024fbdfe29f11d9985e0ab37785c8d
egee-irl <brian@egee.io>
2019-06-30 16:22:44 -0700

    compare the actual announcement message

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -17,8 +17,9 @@ async function Listen(discord: Client, redis: Redis.Redis) {
   const guild = await GetGuild(discord, process.env.SERVER_ID!);
 
   redis.on("message", async (channel, message) => {
+    const announcement = `**${message}** has entered the server`;
     const minecraft = await GetChannel(guild, process.env.MINECRAFT_CHANNEL!);
-    const duplicateMessage = await SameLastMessage(minecraft, message);
+    const duplicateMessage = await SameLastMessage(minecraft, announcement);
     if (duplicateMessage === false) {
       SendChannelMessage(minecraft, `**${message}** has entered the server`);
       console.log("Receive message %s from channel %s", message, channel);
